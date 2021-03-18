c61082379e65850e777654c71cf0055c3cac4082
Suryansh119 <76390896+Suryansh119@users.noreply.github.com>
2021-03-18 10:46:20 +0530

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-int main()
-{
-    int a = 9,b = 4, c;
+#include <stdio.h>//preprocessor directive to include standard input output function header file
+int main(){//main function body start
     
-    c = a+b;
-    printf("a+b = %d\n",c);
-    c = a-b;
-    printf("a-b = %d\n",c);
-    c = a*b;
-    printf("a*b = %d\n",c);
-    c = a/b;
-    printf("a/b = %d\n",c);
-    c = a%b;
-    printf("Remainder = %d\n",c);
+    int a = 9,b = 4, c; //variable declaration and value assignment
     
-    return 0;
-}
+    c = a+b; //logic for adding two numbers
+    printf("a+b = %d\n",c); //printf statement to print addition output
+    c = a-b;//logic for subtracting two numbers
+    printf("a-b = %d\n",c);//printf statement to print subtraction output
+    c = a*b;//logic for multiplying two numbers
+    printf("a*b = %d\n",c);//printf statement to print multiplication output
+    c = a/b;//logic for dividing two numbers
+    printf("a/b = %d\n",c);//printf statement to print division output
+    c = a%b;//logic for remainder 
+    printf("Remainder = %d\n",c);//printf statement to print remainder output
+    
+    return 0;//return statement
+}//main function body ends
